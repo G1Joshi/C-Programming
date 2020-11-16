ed95bc2b727710e997d70431a668389a5995ed72
G1Joshi <joshijeevan98@gmail.com>
2020-11-16 07:04:57 +0530

    clear bit

---
+++ Clear Bit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int clearBit(int n, int k) {
+    return n & ~(1<<k-1);
+}
+
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    printf("%d",clearBit(n, k));
+    return 0;
+}
